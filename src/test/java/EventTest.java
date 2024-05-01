9327bf5f86d7cfe244a570f26cfcbfc9d9d34e7b
Corban Surtees <surco973@student.otago.ac.nz>
2024-05-01 14:26:39 +1200

    test

---
+++ src/test/java/EventTest.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import domain.Event;
+
+public class EventTest {
+    
+    public Event event1;
+    public Event event2;
+    public Event event3;
+    
+    
+    
+    @BeforeEach
+    public void setUp() throws IOException{
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testEvent() {
+        assertTrue(true);
+    }
+}
