145f66790190a018c59def2116577ae9aa07cb47
mayry685 <mayry685@student.otago.ac.nz>
2024-05-10 15:43:59 +1200

    trying to do a fancy tab display thing not loading

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,27 @@
+function openAttribute(attributeName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(attributeName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
 // create the Vue controller
 const app = Vue.createApp({
     mixins: [BasicAccessAuthentication],
@@ -13,6 +37,8 @@ const app = Vue.createApp({
         account: 'account'
     })
 });
+
+
 
 import { BasicAccessAuthentication } from './authentication.js';
 // import the navigation menu
