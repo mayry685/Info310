a05a9752b6a9f36a9fb02abc7363c58c1a41dbd0
mayry685 <mayry685@student.otago.ac.nz>
2024-05-21 12:28:03 +1200

    trying to make the middle tab auto active - nope

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,10 @@ const app = Vue.createApp({
     openAttribute(evt, attributeName) {
       // Declare all variables
       var i, tabcontent, tablinks;
+
+      
     
+
       // Get all elements with class="tabcontent" and hide them
       tabcontent = document.getElementsByClassName("tabcontent");
       for (i = 0; i < tabcontent.length; i++) {
